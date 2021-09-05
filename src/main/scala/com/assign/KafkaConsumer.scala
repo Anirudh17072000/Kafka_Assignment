bef2b80019c8e91b1304759c9c8a124046de8fb0
Anirudh17072000 <anirudh.chauhan@knoldus.com>
2021-09-05 14:28:51 +0530

    New Commit

---
+++ src/main/scala/com/assign/KafkaConsumer.scala
@@ -0,0 +1,57 @@
+
+package com.assign
+import java.io.{BufferedWriter, File, FileWriter}
+import java.util.{Collections, Properties}
+import scala.collection.JavaConverters._
+import org.apache.kafka.clients.consumer.{ConsumerRecords, KafkaConsumer}
+
+object KafkaConsumer extends App {
+
+  val props = new Properties()
+
+  props.put("bootstrap.servers", "localhost:9092")
+
+  props.put(
+    "key.deserializer",
+    "org.apache.kafka.common.serialization.StringDeserializer"
+  )
+
+  /**
+   * Deserializer class for value that implements the Deserializer interface.
+   */
+  props.put(
+    "value.deserializer",
+    "org.apache.kafka.common.serialization.StringDeserializer"
+  )
+
+  props.put("group.id", "consumer-group-1")
+
+  props.put("enable.auto.commit", "true")
+
+  props.put("auto.commit.interval.ms", "1000")
+  props.put("auto.offset.reset", "earliest")
+
+  props.put("session.timeout.ms", "30000")
+
+  val topic = "kafka-topic"
+
+  val consumer: KafkaConsumer[Nothing, String] =
+    new KafkaConsumer[Nothing, String](props)
+
+  /**
+   * Subscribe to the given list of topics to get dynamically assigned partitions.
+   */
+  consumer.subscribe(Collections.singletonList(topic))
+  println("Consuming")
+
+  while (true) {
+    val records: ConsumerRecords[Nothing, String] = consumer.poll(100)
+    for (record <- records.asScala) {
+      val bw = new BufferedWriter(new FileWriter("target/details.txt", true))
+      bw.write("\n" + record.value)
+      bw.close
+      println(record.value)
+    }
+  }
+
+}
